d84c6b070ff8df4e0e103900232414b321467520
woong-sung <cjswo138@gmail.com>
2023-02-01 14:29:35 +0900

    feat: Spring 초기 셋팅

---
+++ server/dailyAuction/src/main/java/com/project/dailyAuction/DailyAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.project.dailyAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyAuctionApplication.class, args);
+	}
+
+}
---
+++ server/dailyAuction/src/test/java/com/project/dailyAuction/DailyAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dailyAuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
