da84765c850943baa4018370266d7ccb94c605be
woong-sung <cjswo138@gmail.com>
2023-02-01 16:36:33 +0900

    feat: member 기본 엔티티 생성

---
+++ server/src/main/java/com/project/dailyAuction/member/entity/Member.java
@@ -0,0 +1,25 @@
+package com.project.dailyAuction.member.entity;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long memberId;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private int coin;
+}
