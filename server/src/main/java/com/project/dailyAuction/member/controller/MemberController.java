22ca6725c12825d46e60aa352914834ac9d788b6
woong-sung <cjswo138@gmail.com>
2023-02-02 13:47:26 +0900

    feat: memberPost 컨트롤러쪽 구현

--- server/src/main/java/com/project/dailyAuction/member/controller/MemberController.java
+++ server/src/main/java/com/project/dailyAuction/member/controller/MemberController.java
@@ -1,14 +1,23 @@
 package com.project.dailyAuction.member.controller;
 
+import com.project.dailyAuction.member.dto.MemberDto;
+import com.project.dailyAuction.member.mapper.MemberMapper;
+import com.project.dailyAuction.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
+    private final MemberRepository memberRepository;
+    private final MemberMapper mapper;
+    // post 회원가입 (이메일, 비밀번호 필요)
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.OK)
+    public void postMember(@RequestBody MemberDto.Signup dto){
 
 }
