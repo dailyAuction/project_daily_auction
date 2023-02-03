d8f2e015bf4a5b77ae892e6ffc98f9d17101f829
woong-sung <cjswo138@gmail.com>
2023-02-03 16:50:54 +0900

    test: 메일전송 테스트 완료

--- server/src/main/java/com/project/dailyAuction/etcService/EmailService.java
+++ server/src/main/java/com/project/dailyAuction/etcService/EmailService.java
@@ -29,12 +29,12 @@ public class EmailService {
 
     public void findPassword(String address, String password) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
-        message.addRecipients(MimeMessage.RecipientType.TO, address);
-        message.setFrom(FROM_ADDRESS);
+        message.addRecipients(MimeMessage.RecipientType.TO, "cjswo138@gmail.com");
+        message.setFrom("yeogiyo007@gmail.com");
         message.setSubject("안녕하세요, 데일리옥션입니다.");
-        message.setText("임시 비밀번호는 다음과 같습니다." +
+        message.setText("<div>임시 비밀번호는 다음과 같습니다.\n" +
                         password+
-                        "로그인 후 비밀번호를 꼭 변경해주세요"
+                        "\n로그인 후 비밀번호를 꼭 변경해주세요<div>"
                 ,"utf-8","html");
         mailSender.send(message);
     }
