c0027d11ca35ce51bdf3be9362b19f44511eeea1
woong-sung <cjswo138@gmail.com>
2023-02-03 15:43:52 +0900

    feat: ExceptionCode 추가

--- server/src/main/java/com/project/dailyAuction/code/ExceptionCode.java
+++ server/src/main/java/com/project/dailyAuction/code/ExceptionCode.java
@@ -1,4 +1,18 @@
 package com.project.dailyAuction.code;
 
+import lombok.Getter;
+
+@Getter
 public enum ExceptionCode {
+    이름(400,"메세지"),
+    MEMBER_EXISTS(409,"Member Exists"),
+    NOT_VERIFIED(409,"Not Verified")
+    ;
+    private final int code;
+    private final String message;
+
+    ExceptionCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 }
