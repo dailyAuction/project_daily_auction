e820c26f82d7b11ef39199836b356610d8b8e8b2
woong-sung <cjswo138@gmail.com>
2023-02-06 16:27:32 +0900

    feat: 에러코드 추가

--- server/src/main/java/com/project/dailyAuction/code/ExceptionCode.java
+++ server/src/main/java/com/project/dailyAuction/code/ExceptionCode.java
@@ -1,4 +1,16 @@
 package com.project.dailyAuction.code;
 
+import lombok.Getter;
+
+@Getter
 public enum ExceptionCode {
+    EXPIRED_TOKEN(401, "Expired Token"),
+    MEMBER_NOT_FOUND(404, "Member not found");
+
+    private final int status;
+    private final String message;
+    ExceptionCode(int statusCode, String message) {
+        this.status = statusCode;
+        this.message = message;
+    }
 }
