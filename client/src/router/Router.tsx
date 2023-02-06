d114a8f5f78c3fa9cfe0487b103e4fd725467687
Hyejung Park <hyejj19@naver.com>
2023-02-06 14:50:18 +0900

    design : NotificationPage 생성 및 라우터 추가

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CategoryPage } from '../pages/Category';
+import { CategoryPage } from '../pages/CategoryPage';
 import { DetailPage } from '../pages/DetailPage';
 import { MyPage } from '../pages/MyPage';
+import { NotificationPage } from '../pages/NoficationPage';
 import { PostProductPage } from '../pages/PostProductPage';
-import { SearchPage } from '../pages/Search';
+import { SearchPage } from '../pages/SearchPage';
 import { TempPage } from '../pages/TempPage';
 
 export const Router = () => {
@@ -16,6 +17,7 @@ export const Router = () => {
         <Route path="/search" element={<SearchPage />} />
         <Route path="/postProduct" element={<PostProductPage />} />
         <Route path="/my" element={<MyPage />} />
+        <Route path="/notification" element={<NotificationPage />} />
       </Routes>
     </BrowserRouter>
   );
