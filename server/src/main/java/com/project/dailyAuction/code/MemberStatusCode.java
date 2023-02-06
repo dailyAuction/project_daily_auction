da14dad002459966f69c96c513ae3e13aaada399
woong-sung <cjswo138@gmail.com>
2023-02-06 16:34:33 +0900

    refactor: memberstatuscode 수정

--- server/src/main/java/com/project/dailyAuction/code/MemberStatusCode.java
+++ server/src/main/java/com/project/dailyAuction/code/MemberStatusCode.java
@@ -1,7 +1,15 @@
 package com.project.dailyAuction.code;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum MemberStatusCode {
-    활동회원,
-    휴면회원,
-    탈퇴회원
+    활동회원(001,"활동회원"),
+    휴면회원(002,"휴면회원"),
+    탈퇴회원(003,"탈퇴회원");
+    private final int code;
+    private final String status;
+
 }
