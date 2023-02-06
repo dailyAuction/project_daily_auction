3f16123266731d33e30e7539e699d36ae47cc2f5
woong-sung <cjswo138@gmail.com>
2023-02-06 16:30:02 +0900

    feat: 스프링 시큐리티

---
+++ server/src/main/java/com/project/dailyAuction/security/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.project.dailyAuction.security.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginDto {
+    private String email;
+    private String password;
+}
---
+++ server/src/main/java/com/project/dailyAuction/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,74 @@
+package com.project.dailyAuction.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.dailyAuction.member.entity.Member;
+import com.project.dailyAuction.security.dto.LoginDto;
+import com.project.dailyAuction.security.jwt.JwtTokenizer;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenizer jwtTokenizer;
+
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+
+        //로그인 dto를 역직렬화
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+
+        // 이메일과 비번을 포함한 토큰 생성
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws ServletException, IOException {
+        // principal로부터 member정보 호출
+        Member member = (Member) authResult.getPrincipal();
+
+        // 토큰 생성
+        String accessToken = delegateAccessToken(member);
+
+        response.setHeader("AccessToken", "bearer " + accessToken);
+
+        this.getSuccessHandler().onAuthenticationSuccess(request,response,authResult);
+    }
+
+    // 엑세스토큰 생성 로직
+    private String delegateAccessToken(Member member) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", member.getEmail());
+        claims.put("userId",member.getMemberId());
+
+        String subject = member.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+}
