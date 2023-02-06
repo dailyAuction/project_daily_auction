8a578a52684b766ddefd3eadd7ddcc5baf8822db
woong-sung <cjswo138@gmail.com>
2023-02-06 16:28:49 +0900

    feat: 패스워드 인코더, 토큰으로 멤버찾기, oauth등록 위한 메서드 추가

--- server/src/main/java/com/project/dailyAuction/member/service/MemberService.java
+++ server/src/main/java/com/project/dailyAuction/member/service/MemberService.java
@@ -1,11 +1,16 @@
 package com.project.dailyAuction.member.service;
 
 
+import com.project.dailyAuction.code.ExceptionCode;
 import com.project.dailyAuction.member.dto.MemberDto;
 import com.project.dailyAuction.member.entity.Member;
 import com.project.dailyAuction.member.repository.MemberRepository;
+import com.project.dailyAuction.security.jwt.JwtTokenizer;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
@@ -15,6 +20,7 @@ import java.util.Optional;
 @Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final JwtTokenizer jwtTokenizer;
 
     // 멤버 저장
     public Member save(MemberDto.SignIn dto){
@@ -22,8 +28,7 @@ public class MemberService {
 
         return memberRepository.save(Member.builder()
                 .email(dto.getEmail())
-                //todo: 패스워드 암호화 필요
-                .password(dto.getPassword())
+                .password(passwordEncoder().encode(dto.getPassword()))
                 .coin(0)
                 .build()
         );
@@ -33,23 +38,23 @@ public class MemberService {
     public Member update(String accessToken, MemberDto.Update dto){
         Member member = findByAccessToken(accessToken);
         verifyPassword(member, dto.getCurrentPassword());
-//        String newPassword = passwordEncoder().encode(dto.getNewPassword())
-        member.changePassword(dto.getNewPassword());
+        String newPassword = passwordEncoder().encode(dto.getNewPassword());
+        member.changePassword(newPassword);
 
         return memberRepository.save(member);
     }
 
     // 비밀번호 체크
     public void verifyPassword(Member member, String password){
-//        if (!passwordEncoder().matches(password, member.getPassword())) {
-//            throw new IllegalArgumentException();
-//        }
+        if (!passwordEncoder().matches(password, member.getPassword())) {
+            throw new IllegalArgumentException();
+        }
 
     }
 
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     // 존재하는 이메일 체크
     public void verifyExistEmail(String email){
@@ -59,17 +64,42 @@ public class MemberService {
             throw new IllegalArgumentException();
         }
     }
+    public Member find(long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new ResponseStatusException(ExceptionCode.MEMBER_NOT_FOUND.getStatus(), ExceptionCode.MEMBER_NOT_FOUND.getMessage(), new IllegalArgumentException()));
+    }
 
     // 액세스토큰으로 멤버 찾기
     public Member findByAccessToken(String accessToken){
-        //todo:
-        return null;
+        long memberId = jwtTokenizer.getMemberId(accessToken);
+        return find(memberId);
     }
 
     // 이메일로 멤버 찾기
     public Member findByEmail(String email) {
         Optional<Member> optionalMember = memberRepository.findByEmail(email);
         if (optionalMember.isPresent()) throw new IllegalArgumentException();
+
+        return optionalMember.get();
+    }
+
+    // 이메일로 멤버 수 체크
+    public int countByEmail(String email){
+        return memberRepository.countByEmail(email);
+    }
+
+    public Member saveOauthMember(String email) {
+        return memberRepository.save(Member.builder()
+                .email(email)
+                .password(passwordEncoder().encode("epdlffldhrtusthtufqlalfqjsgh"))
+                .coin(0)
+                .build()
+        );
+    }
+
+    public Member findByEmailForOauth(String email) {
+        Optional<Member> optionalMember = memberRepository.findByEmail(email);
+        if (!optionalMember.isPresent()) return null;
 
         return optionalMember.get();
     }
