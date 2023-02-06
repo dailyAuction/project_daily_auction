1482bdace1ae5139abf08085013cacde51ea5029
casava840 <hen3403@gmail.com>
2023-02-06 14:16:11 +0900

    feat: BoardMember 기본세팅

---
+++ server/src/main/java/com/project/dailyAuction/boardMember/entity/BoardMember.java
@@ -0,0 +1,31 @@
+package com.project.dailyAuction.boardMember.entity;
+
+import com.project.dailyAuction.board.entity.Board;
+import com.project.dailyAuction.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class BoardMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long boardMemberId;
+
+    @Column
+    private int myPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
---
+++ server/src/main/java/com/project/dailyAuction/boardMember/repository/BoardMemberRepository.java
@@ -0,0 +1,7 @@
+package com.project.dailyAuction.boardMember.repository;
+
+import com.project.dailyAuction.boardMember.entity.BoardMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardMemberRepository extends JpaRepository<BoardMember, Long> {
+}
