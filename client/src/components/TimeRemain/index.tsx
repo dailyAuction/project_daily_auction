f6a5ec85277775e12a8a7d91cfe03b7314e8c13a
jaehoon <halmokme@gmail.com>
2023-02-06 17:47:10 +0900

    design: 상품아이템 이미지

--- client/src/components/TimeRemain/index.tsx
+++ client/src/components/TimeRemain/index.tsx
@@ -4,8 +4,8 @@ export const TimeRemain = ({ children }) => {
   return (
     <button
       type="button"
-      className={`rounded-[10px] px-2.5 shadow-lg w-[70px] h-[18px] opacity-40 text-xs ${
-        oneHourLeft ? 'bg-[E4E4E4] text-main-red' : 'bg-black text-white'
+      className={`rounded-[10px] px-2.5 shadow-lg w-[70px] h-[18px] text-xs ${
+        oneHourLeft ? 'bg-[#E4E4E4] text-main-red' : 'bg-black text-white opacity-40'
       } `}>
       {children}
     </button>
