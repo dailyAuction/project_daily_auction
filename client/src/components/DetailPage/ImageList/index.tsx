d3e0ead19acc1dcfc38f226a064573d68a8ec1eb
Hyejung Park <hyejj19@naver.com>
2023-02-07 14:41:44 +0900

    design : 모달 컴포넌트 생성, 이미지 포인터 커서 추가

--- client/src/components/DetailPage/ImageList/index.tsx
+++ client/src/components/DetailPage/ImageList/index.tsx
@@ -1,8 +1,13 @@
 export const ImageList = ({ url }) => {
   return (
     <section className="flex h-[150px] justify-cente w-full items-center overflow-x-scroll space-x-3">
-      {url.map((src: string) => (
-        <img key={src} src={src} className="flex-shrink-0 w-28 h-28 rounded-md object-cover" alt="상품 상세1" />
+      {url.map((src: string, idx: number) => (
+        <img
+          key={src}
+          src={src}
+          className="flex-shrink-0 w-28 h-28 rounded-md object-cover cursor-pointer"
+          alt={` ${idx + 1} 번째 상품 상세 이미지`}
+        />
       ))}
     </section>
   );
