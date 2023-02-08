e82d5bbcc93a54da9b276b8616c9034ad095bf27
Hyejung Park <hyejj19@naver.com>
2023-02-08 15:35:17 +0900

    feat : Notification 게시글 이동, 삭제 함수 구현
    * 삭제의 경우 react-query 를 사용할 예정이므로 내부 로직은 아직 구현하지 않았습니다.~

--- client/src/components/NotificationPage/Notification/index.tsx
+++ client/src/components/NotificationPage/Notification/index.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import { NOTIFICATION_STATUS, NOTIFICATION_STATUS_MSG } from '../../../constants/constants';
+import { useNotification } from './useNotification';
 
 type NotificationProps = {
   noticeId: number;
@@ -10,11 +12,12 @@ type NotificationProps = {
 };
 
 export const Notification = ({ noticeId, boardId, boardTitle, image, status, contact }: NotificationProps) => {
+  const { handleDelete } = useNotification();
   return (
     <section className="bg-white mt-2 py-2 px-3 rounded-lg space-y-3">
       <article className="flex justify-between items-center border-b pt-2 pb-1">
         <span>{NOTIFICATION_STATUS[status]} 알림</span>
-        <i>
+        <i onClick={() => handleDelete(noticeId)}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -26,29 +29,31 @@ export const Notification = ({ noticeId, boardId, boardTitle, image, status, con
           </svg>
         </i>
       </article>
-      <article>
-        <h1 className="text-lg font-bold">
-          {!status ? contact + NOTIFICATION_STATUS_MSG[status] : NOTIFICATION_STATUS_MSG[status]}
-        </h1>
-        {!status && <span className="text-sm">* 위 이메일로 연락해주세요.</span>}
-      </article>
+      <Link to={`/detail/${boardId}`} className="flex flex-col space-y-2">
+        <article>
+          <h1 className="text-lg font-bold">
+            {!status ? contact + NOTIFICATION_STATUS_MSG[status] : NOTIFICATION_STATUS_MSG[status]}
+          </h1>
+          {!status && <span className="text-sm">* 위 이메일로 연락해주세요.</span>}
+        </article>
 
-      <article className="flex items-center space-x-2 relative">
-        <img src={image} alt="상품 썸네일 이미지" className="w-10 h-10 rounded-md" />
-        {/* TODO:  20글자 이상 넘어가면 ...으로 표시 */}
-        <span className="text-sm">{boardTitle.length > 20 ? boardTitle.slice(0, 20) + '...' : boardTitle}</span>
-        <i>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-4 h-4 absolute right-0 bottom-3">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </i>
-      </article>
+        <article className="flex items-center space-x-2 relative">
+          <img src={image} alt="상품 썸네일 이미지" className="w-10 h-10 rounded-md" />
+          {/* TODO:  20글자 이상 넘어가면 ...으로 표시 */}
+          <span className="text-sm">{boardTitle.length > 20 ? boardTitle.slice(0, 20) + '...' : boardTitle}</span>
+          <i>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-4 h-4 absolute right-0 bottom-3">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+          </i>
+        </article>
+      </Link>
     </section>
   );
 };
