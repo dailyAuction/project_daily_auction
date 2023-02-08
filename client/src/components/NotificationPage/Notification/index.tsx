6aaaf4bcae46e157a2d22770106aab5ac1f0a2c8
Hyejung Park <hyejj19@naver.com>
2023-02-08 15:23:37 +0900

    design : Notification 페이지 및 컴포넌트 생성

---
+++ client/src/components/NotificationPage/Notification/index.tsx
@@ -0,0 +1,54 @@
+import { NOTIFICATION_STATUS, NOTIFICATION_STATUS_MSG } from '../../../constants/constants';
+
+type NotificationProps = {
+  noticeId: number;
+  boardId: number;
+  boardTitle: string;
+  image: string;
+  status: number;
+  contact?: string;
+};
+
+export const Notification = ({ noticeId, boardId, boardTitle, image, status, contact }: NotificationProps) => {
+  return (
+    <section className="bg-white mt-2 py-2 px-3 rounded-lg space-y-3">
+      <article className="flex justify-between items-center border-b pt-2 pb-1">
+        <span>{NOTIFICATION_STATUS[status]} 알림</span>
+        <i>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </i>
+      </article>
+      <article>
+        <h1 className="text-lg font-bold">
+          {!status ? contact + NOTIFICATION_STATUS_MSG[status] : NOTIFICATION_STATUS_MSG[status]}
+        </h1>
+        {!status && <span className="text-sm">* 위 이메일로 연락해주세요.</span>}
+      </article>
+
+      <article className="flex items-center space-x-2 relative">
+        <img src={image} alt="상품 썸네일 이미지" className="w-10 h-10 rounded-md" />
+        {/* TODO:  20글자 이상 넘어가면 ...으로 표시 */}
+        <span className="text-sm">{boardTitle.length > 20 ? boardTitle.slice(0, 20) + '...' : boardTitle}</span>
+        <i>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-4 h-4 absolute right-0 bottom-3">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+        </i>
+      </article>
+    </section>
+  );
+};
