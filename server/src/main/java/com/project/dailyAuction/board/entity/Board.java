5751a89db92c57e4b60f636d5ae8fde1b7563ca0
woong-sung <cjswo138@gmail.com>
2023-02-08 17:18:03 +0900

    refactor: status->statusId

--- server/src/main/java/com/project/dailyAuction/board/entity/Board.java
+++ server/src/main/java/com/project/dailyAuction/board/entity/Board.java
@@ -31,7 +31,7 @@ public class Board {
     @Column
     private String thumbnail;
     @Column
-    private String status;
+    private long statusId;
     @Column
     private String category;
     @Column
