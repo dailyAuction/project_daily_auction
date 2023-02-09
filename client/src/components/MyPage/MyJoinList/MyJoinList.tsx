47a462db47a7093f60ac60d106a968c866aaeac3
jaehoon <halmokme@gmail.com>
2023-02-10 04:07:37 +0900

    design 내가 참여한 경매페이지

--- client/src/components/MyPage/MyJoinList/MyJoinList.tsx
+++ client/src/components/MyPage/MyJoinList/MyJoinList.tsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+import { products } from '../../../mock/product';
 import { SubHeader } from '../../_common/Header/SubHeader/SubHeader';
+import { TabBar } from '../../_common/TabBar/TabBar';
+import { MyAuctionBtn } from '../MyAuctionBtn/MyAuctionBtn';
+import { MyAuctionContent } from '../MyAuctionContent/MyAuctionContent';
 
 export const MyJoinList = () => {
+  const [status, setStatus] = useState(0);
+  const [details] = useState(products);
+
   return (
     <div className="base-layout">
       <SubHeader>내가 참여한 경매</SubHeader>
+      <MyAuctionBtn status={status} setStatus={setStatus} />
+      <MyAuctionContent details={details} status={status} />
+      <TabBar />
     </div>
   );
 };
