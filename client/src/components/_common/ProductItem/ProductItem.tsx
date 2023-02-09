ce66ac81f1e8a89247990ac6b9ba53eb7973e62e
Hyejung Park <hyejj19@naver.com>
2023-02-09 16:47:40 +0900

    feat : ProductItem 컴포넌트 Props 및 기능 연결

--- client/src/components/_common/ProductItem/ProductItem.tsx
+++ client/src/components/_common/ProductItem/ProductItem.tsx
@@ -1,5 +1,8 @@
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { AUCTION_STATUS } from '../../../constants/constants';
+import { useIsMatchUserId } from '../../../hooks/useIsMatchUserId';
 import { ProductDetailResp } from '../../../types/product.type';
+import { getShortString } from '../../../utils/getShortString';
 import { ProductItemImg } from '../ProductItemImg/ProductItemImg';
 import { ProductStatus } from '../ProductStatus/ProductStatus';
 
@@ -8,9 +11,11 @@ type ProductItemProps = {
 };
 
 export const ProductItem = ({ productDetail }: ProductItemProps) => {
-  const { boardId, thumbnail, title, currentPrice, statusId, createdAt, finishedAt } = productDetail;
+  const { boardId, thumbnail, title, currentPrice, statusId, createdAt, finishedAt, authorId } = productDetail;
 
-  const isLoginUser = true;
+  // 현재 유저가 Seller 인지 판단합니다.
+  const { isMatchUserId } = useIsMatchUserId();
+  const isUserSeller = isMatchUserId(authorId);
 
   const location = useLocation().pathname;
   const page = location.includes('my-auction-list')
@@ -20,32 +25,32 @@ export const ProductItem = ({ productDetail }: ProductItemProps) => {
     : '';
 
   return (
-    <div className="flex justify-center items-end relative w-full rounded-[10px] bg-background-mobile">
-      <div className="p-[6px] flex-2">
-        <ProductItemImg />
-      </div>
-      <div className="flex-1 px-0.5">
-        {statusId === 1 && (
-          <div className="text-xs font-bold py-2">
-            <p>판매자 이메일</p>
-            <p>:aaaa@aaaa.com</p>
+    <Link to={`/detail/${boardId}`}>
+      <div className="flex justify-center items-end relative w-full rounded-[10px] bg-background-mobile cursor-pointer">
+        <div className="p-[6px] flex-2">
+          {/* TODO: 남은 시간 데이터 props 전달 */}
+          <ProductItemImg thumbnail={thumbnail} />
+        </div>
+        <div className="flex-1 px-0.5">
+          {statusId === 1 && (
+            <div className="text-xs font-bold py-2">
+              <p>판매자 이메일</p>
+              <p>:aaaa@aaaa.com</p>
+            </div>
+          )}
+          <p className="text-base font-bold line-clamp-2">{getShortString(title, 40)}</p>
+          <div className="pb-2 pt-3 text-xs">
+            {isUserSeller && statusId !== 0 ? '' : page === 'register' ? '시작가 10000 coin' : '입찰가 10000 coin'}
+            <div className="flex items-center gap-0.5">
+              {isUserSeller && statusId === 0 ? '현재가' : ''}
+              <p className="text-base text-main-orange">150,000 coin</p>
+            </div>
           </div>
-        )}
-        <p className="text-xs line-clamp-2">
-          엄청난 야구모자를 파는 경매를 진행 하고 있는 이 경매는 정말 엄 ...엄청난 야구모자를 파는 경매를 진행 하고있는
-          엄청난 야구모자를 파는 경매를 진행 하고 있는 이 경매는 정말 엄 ...엄청난 야구모자를 파는 경매를 진행 하고
-        </p>
-        <div className="pb-2 pt-3 text-xs">
-          {isLoginUser && statusId !== 0 ? '' : page === 'register' ? '시작가 10000 coin' : '입찰가 10000 coin'}
-          <div className="flex items-center gap-0.5">
-            {isLoginUser && statusId === 0 ? '현재가' : ''}
-            <p className="text-base text-main-orange">150,000 coin</p>
+          <div className="absolute top-2 right-2">
+            <ProductStatus>{statusId === 0 ? AUCTION_STATUS[statusId] : ''}</ProductStatus>
           </div>
         </div>
-        <div className="absolute top-2 right-2">
-          <ProductStatus>{statusId === 0 ? statusId : ''}</ProductStatus>
-        </div>
       </div>
-    </div>
+    </Link>
   );
 };
--- client/src/pages/SearchResultPage.tsx
+++ client/src/pages/SearchResultPage.tsx
@@ -2,12 +2,14 @@ import { TabBar } from '../components/_common/TabBar/TabBar';
 import { SearchQuery } from '../components/_common/SearchQuery/SearchQuery';
 import { MainHeader } from '../components/_common/Header/MainHeader/MainHeader';
 import { SearchResultList } from '../components/SearchResultPage/SearchResultList/SearchResultList';
+import { useGetQueryString } from '../hooks/useGetQueryString';
+import { getShortString } from '../utils/getShortString';
 
 export const SearchResultPage = () => {
-  const isLoginUser = true;
+  const [categoryId, keyword] = useGetQueryString().split('_');
   return (
     <main className="base-layout">
-      <MainHeader>{`"크라라의 훈장..." 검색 결과`}</MainHeader>
+      <MainHeader>{`"${getShortString(keyword, 10)}" 검색 결과`}</MainHeader>
       <section className="content-layout bg-white border-t space-y-9 px-3">
         <SearchQuery />
         <SearchResultList />
