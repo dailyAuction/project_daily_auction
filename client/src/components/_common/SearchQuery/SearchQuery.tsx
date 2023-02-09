7276807061ea07a66ffcdc6a93aead3003f9ea83
Hyejung Park <hyejj19@naver.com>
2023-02-09 16:02:36 +0900

    feat : 검색 페이지 라우팅 기능 구현
    * 카테고리와 키워드 입력 후 엔터, 돋보기 클릭시 검색 결과 페이지로 이동
    * 검색 결과 페이지에서 쿼리 파라미터 추출하여 api 요청 필요

--- client/src/components/_common/SearchQuery/SearchQuery.tsx
+++ client/src/components/_common/SearchQuery/SearchQuery.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import { CATEGORIES } from '../../../constants/constants';
+import { useGetQueryString } from '../../../hooks/useGetQueryString';
 import { useSearchQuery } from './useSearchQuery';
 
 export const SearchQuery = () => {
@@ -9,7 +9,7 @@ export const SearchQuery = () => {
   const { handleSearch } = useSearchQuery();
 
   // 현재 페이지가 검색 결과 페이지인지 queryString 여부로 판단할 수 있습니다.
-  const queryString = useLocation().pathname.slice(7);
+  const queryString = useGetQueryString();
 
   const handleClick = () => {
     handleSearch(searchCategory, searchKeyword);
