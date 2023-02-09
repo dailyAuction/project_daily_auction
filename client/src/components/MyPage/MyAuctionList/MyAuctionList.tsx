6ad6931ef390685fb6deb2cf27759329cdc05590
jaehoon <halmokme@gmail.com>
2023-02-10 02:19:55 +0900

    design 나의경매페이지 버튼

--- client/src/components/MyPage/MyAuctionList/MyAuctionList.tsx
+++ client/src/components/MyPage/MyAuctionList/MyAuctionList.tsx
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+import { products } from '../../../mock/product';
+import { ProductItem } from '../../_common/\bProductItem/ProductItem';
+import { SubHeader } from '../../_common/Header/SubHeader/SubHeader';
+import { TabBar } from '../../_common/TabBar/TabBar';
+import { MyAuctionBtn } from '../MyAuctionBtn/MyAuctionBtn';
+
 export const MyAuctionList = () => {
+  const [status, setStatus] = useState(0);
+  const [myAuctionList] = useState(products);
+
   return (
     <div className="base-layout">
-      <div> </div>
+      <SubHeader>내가 등록한 경매</SubHeader>
+      {/* button */}
+      <MyAuctionBtn status={status} setStatus={setStatus} />
+
+      {/* itemList */}
+      <div className="h-4/5 overflow-x-auto scrollbar-hide">
+        {myAuctionList.map((el) => {
+          return <ProductItem key={el.boardId} productDetail={el} />;
+        })}
+        {myAuctionList.map((el) => {
+          return <ProductItem key={el.boardId} productDetail={el} />;
+        })}{' '}
+        {myAuctionList.map((el) => {
+          return <ProductItem key={el.boardId} productDetail={el} />;
+        })}
+      </div>
+
+      <TabBar />
     </div>
   );
 };
