f8469ee366b8b3e4b023a9e6257dd4c632b381a9
jaehoon <halmokme@gmail.com>
2023-02-10 03:51:59 +0900

    design 페이지별 버튼 문구 변경

--- client/src/components/MyPage/MyAuctionBtn/MyAuctionBtn.tsx
+++ client/src/components/MyPage/MyAuctionBtn/MyAuctionBtn.tsx
@@ -1,7 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export const MyAuctionBtn = ({ status, setStatus }) => {
-  const [buttonList] = useState(['진행중인 경매', '성공한 경매', '유찰된 경매']);
+  const location = useLocation().pathname;
+  const page = location.includes('auctionList') ? 'auction' : 'join';
+
+  const [buttonList] = useState(
+    page === 'auction'
+      ? ['진행중인 경매', '성공한 경매', '유찰된 경매']
+      : ['진행중인 경매', '낙찰된 경매', '실패한 경매']
+  );
 
   const handlerClickBtn = (idx) => {
     setStatus(idx);
