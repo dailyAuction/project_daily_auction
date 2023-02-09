20c1ec104d3732d161a35ce5e099a396113fddef
raon9401 <raon9401@naver.com>
2023-02-09 17:40:04 +0900

    feat : 카테고리 페이지 리스트 추가

--- client/src/pages/CategoryProductListPage.tsx
+++ client/src/pages/CategoryProductListPage.tsx
@@ -4,20 +4,36 @@ import { TabBar } from '../components/_common/TabBar/TabBar';
 import { MainHeader } from '../components/_common/Header/MainHeader/MainHeader';
 import { CategoryList } from '../components/_common/CategoryList/CategoryList';
 import { CATEGORIES } from '../constants/constants';
+import { ProductItem } from '../components/_common/\bProductItem/ProductItem';
 
 export const CategoryProductListPage = () => {
   const { id } = useParams();
   const [sortState, setSortState] = useState(0);
 
-  // srotState가 변할때 마다 요청 로직 필요
+  // 테스트용
+  const testArr: number[] = [];
+  for (let i = 0; i < 100; i++) {
+    testArr[i] = i;
+  }
+
+  // TODO : srotState가 변할때 마다 요청 로직 필요
   return (
     <main className="base-layout bg-white">
-      <MainHeader>{CATEGORIES[Number(id)]}</MainHeader>
       <section className="w-full">
+        <MainHeader>{CATEGORIES[Number(id)]}</MainHeader>
         <CategoryList sortState={sortState} setSortState={setSortState} />
-        {/* TODO : 상품 리스트 조회 추가 */}
       </section>
-      <TabBar />
+      <section className="w-full overflow-y-scroll scrollbar-hide">
+        {/* TODO : 상품 리스트 조회 추가 */}
+        <article className="px-[10px] space-y-3 mb-[10px]">
+          {testArr.map((item) => (
+            <ProductItem key={item} isLoginUser status={'진행중'} />
+          ))}
+        </article>
+      </section>
+      <section className="w-full">
+        <TabBar />
+      </section>
     </main>
   );
 };
