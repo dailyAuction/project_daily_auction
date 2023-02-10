2a46dec0f43c107a9de1e32216b14ed06220e074
jaehoon <halmokme@gmail.com>
2023-02-10 16:07:12 +0900

    design 상품등록페이지 모달

--- client/src/pages/PostProductPage.tsx
+++ client/src/pages/PostProductPage.tsx
@@ -1,19 +1,29 @@
+import { useState, useRef, useCallback } from 'react';
 import { CategoryDropdown } from '../components/PostProductPage/CategoryDropdown/CategoryDropdown';
 import { RegisterItemImg } from '../components/PostProductPage/RegisterItemImg/RegisterItemImg';
 import { SubHeader } from '../components/_common/Header/SubHeader/SubHeader';
 import { RegisterProductInfo } from '../components/PostProductPage/RegisterProductInfo/RegisterProductInfo';
 import { TabBar } from '../components/_common/TabBar/TabBar';
+import { RegisterBtn } from '../components/PostProductPage/RegisterBtn/RegisterBtn';
 
 export const PostProductPage = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handlerModal = useCallback(() => {
+    setModalOpen(!modalOpen);
+  }, [modalOpen]);
+
   return (
     <main className="base-layout">
-      <SubHeader>상품 등록</SubHeader>
-      <RegisterItemImg />
-      <CategoryDropdown />
-      <RegisterProductInfo />
-      <button type="submit" className="red-btn m-2 ml-auto mb-3">
-        등록
-      </button>
+      <div className="base-layout" onClick={handlerModal}>
+        <SubHeader>상품 등록</SubHeader>
+        <RegisterItemImg />
+        <CategoryDropdown />
+        <RegisterProductInfo />
+
+        <RegisterBtn modalOpen={modalOpen} handlerModal={handlerModal} />
+      </div>
+
       <TabBar />
     </main>
   );
