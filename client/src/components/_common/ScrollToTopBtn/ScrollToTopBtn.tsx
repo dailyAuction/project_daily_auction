c56631088eda0e289747a871e96398a9f0ab2bf4
jaehoon <halmokme@gmail.com>
2023-02-10 16:30:21 +0900

    design 페이지 상단 이동 버튼

---
+++ client/src/components/_common/ScrollToTopBtn/ScrollToTopBtn.tsx
@@ -0,0 +1,15 @@
+export const ScrollToTopBtn = () => {
+  return (
+    <div className="fixed top-[530px] right-5 flex justify-center items-center w-10 h-10 bg-background-mobile rounded-full cursor-pointer border border-[#ababab] hover:opacity-80 z-1000 ">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={3}
+        stroke="black"
+        className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+      </svg>
+    </div>
+  );
+};
