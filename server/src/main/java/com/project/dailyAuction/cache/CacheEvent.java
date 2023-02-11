4e13ef461f5a7ae0b0e10550f7af80608dc3111e
woong-sung <cjswo138@gmail.com>
2023-02-11 20:42:41 +0900

    feat: 캐싱 event

---
+++ server/src/main/java/com/project/dailyAuction/cache/CacheEvent.java
@@ -0,0 +1,38 @@
+package com.project.dailyAuction.cache;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CacheEvent {
+    private final CacheProcessor cacheProcessor;
+
+    // 시작 시
+    @PostConstruct
+    public void initCache(){
+        cacheProcessor.updateViewToMySql();
+        cacheProcessor.flushRedis();
+    }
+
+    // 순서별 정리
+    //1. 초(0-59)
+    //2. 분(0-59)
+    //3. 시간(0-23)
+    //4. 일(1-31)
+    //5. 월(1-12)
+    //6. 요일(0-7)
+//    @Scheduled(cron = "0 0 0/1 * * ?")
+    @Scheduled(cron = "*/10 * * * * ?")
+    public void scheduleCache() {
+
+        cacheProcessor.updateViewToMySql();
+        cacheProcessor.flushRedis();
+        log.info("플러시 레디스");
+    }
+}
