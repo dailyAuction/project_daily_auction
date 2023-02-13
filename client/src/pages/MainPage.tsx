80fcb0471d56ff46f3e97a5263b3042d00743496
jaehoon <halmokme@gmail.com>
2023-02-13 20:43:23 +0900

    chore 메인페이지 컴포넌트 props 제거

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Banner } from '../components/MainPage/Banner/Banner';
 import { Bestproduct } from '../components/MainPage/BestProduct/BestProduct';
 import { ClosingProduct } from '../components/MainPage/ClosingProduct/ClosingProduct';
@@ -7,18 +6,14 @@ import { ToList } from '../components/MainPage/ToList/ToList';
 import { MainHeader } from '../components/_common/Header/MainHeader/MainHeader';
 import { ScrollToTopBtn } from '../components/_common/ScrollToTopBtn/ScrollToTopBtn';
 import { TabBar } from '../components/_common/TabBar/TabBar';
-import { products } from '../mock/product';
 
 export const MainPage = () => {
-  const [closingProductDetail] = useState(products);
-  const [bestProductDetail] = useState(products);
-
   return (
     <main className="base-layout bg-white relative">
       <MainHeader>Daily Auction</MainHeader>
       <Banner />
-      <ClosingProduct closingProductDetail={closingProductDetail} />
-      <Bestproduct bestProductDetail={bestProductDetail} />
+      <ClosingProduct />
+      <Bestproduct />
       <ToList />
       <Footer />
       <div className="fixed bottom-0 sm:w-[500px] w-screen">
