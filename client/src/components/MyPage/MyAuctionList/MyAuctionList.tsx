16753cb09b2a8af6bc2f3dda7d5b592576822036
jaehoon <halmokme@gmail.com>
2023-02-14 14:15:25 +0900

    feat 내가 등록한 경매페이지 통신로직 작성

--- client/src/components/MyPage/MyAuctionList/MyAuctionList.tsx
+++ client/src/components/MyPage/MyAuctionList/MyAuctionList.tsx
@@ -1,19 +1,40 @@
 import { useState } from 'react';
-import { products } from '../../../mock/product';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import { ProductDetailResp } from '../../../types/product.type';
 import { SubHeader } from '../../_common/Header/SubHeader/SubHeader';
 import { TabBar } from '../../_common/TabBar/TabBar';
 import { MyAuctionBtn } from '../MyAuctionBtn/MyAuctionBtn';
 import { MyAuctionContent } from '../MyAuctionContent/MyAuctionContent';
+import { AUCTION_STATUS } from '../../../constants/constants';
 
 export const MyAuctionList = () => {
   const [status, setStatus] = useState(0);
-  const [details] = useState(products);
+
+  const getMyAuction = async () => {
+    const { data } = await axios.get<ProductDetailResp[]>(
+      `${process.env.REACT_APP_URL}/members/${status}/my-auction-list`
+    );
+    return data;
+  };
+
+  const {
+    data: myAuctionList,
+    isLoading,
+    isError,
+  } = useQuery<ProductDetailResp[], Error>('bestProduct', getMyAuction, {
+    staleTime: 1000 * 20,
+    retry: 0,
+    onError: (e) => console.log(e.message),
+  });
 
   return (
     <div className="base-layout bg-white">
       <SubHeader>내가 등록한 경매</SubHeader>
       <MyAuctionBtn status={status} setStatus={setStatus} />
-      <MyAuctionContent details={details} status={status} />
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>{`${AUCTION_STATUS[status]} 상품이 없습니다.`}</div>}
+      <MyAuctionContent details={myAuctionList} status={status} />
       <TabBar />
     </div>
   );
