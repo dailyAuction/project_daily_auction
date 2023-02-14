b319ab5ab075385f9ed03e43d18bd75dc265f44d
raon9401 <raon9401@naver.com>
2023-02-14 15:33:02 +0900

    feat : 회원가입 이메일 인증 요청 모달 추가

--- client/src/components/SignUpPage/SignUp/SignUp.tsx
+++ client/src/components/SignUpPage/SignUp/SignUp.tsx
@@ -3,6 +3,8 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
+import { AuthModal } from '../SignUpModal/AuthModal';
+import { REG_EMAIL, REG_PASSWORD } from '../../../constants/constants';
 
 interface SignUpData {
   email: string;
@@ -12,11 +14,12 @@ interface SignUpData {
 
 export const SignUp = () => {
   const navigate = useNavigate();
+  const [isModalOpen, setModalOpen] = useState(false);
 
   const [isConfirm, setIsConfirm] = useState(true);
   const [isCheck, setIsCheck] = useState(false);
   const [verify, setVerify] = useState(false);
-  const [verifyEmailReg, setVerifyEmailReg] = useState(false);
+  const [verifyEmailReg, setVerifyEmailReg] = useState<1 | 0 | -1>(0);
   const [verifyCode, setVerifyCode] = useState('');
 
   // 임시 인증번호 통신 가능시 삭제
@@ -30,7 +33,6 @@ export const SignUp = () => {
   } = useForm<SignUpData>();
 
   const onSubmit = handleSubmit((data: SignUpData) => {
-    console.log('굿');
     // confirm data가 같은지 확인.
     if (data.password !== data.confirmPassword) {
       setIsConfirm(false);
@@ -42,14 +44,11 @@ export const SignUp = () => {
 
   const getAuthVerify = () => {
     const { email } = getValues();
-    const exptext = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-
-    console.log('이메일 인증 요청', verifyEmailReg);
-    if (exptext.test(email)) {
-      // 이때만 요청 보냄.
-      setVerifyEmailReg(true);
+    if (REG_EMAIL.test(email)) {
+      setVerifyEmailReg(1);
+      setModalOpen(true);
     } else {
-      setVerifyEmailReg(false);
+      setVerifyEmailReg(-1);
     }
   };
 
@@ -62,113 +61,117 @@ export const SignUp = () => {
   };
 
   return (
-    <main className="w-full flex flex-col mt-11 items-center space-y-5">
-      <section className="space-y-7 w-4/5">
-        <form className="space-y-2" onSubmit={onSubmit}>
-          <article>
-            <span className="text-sm">이메일</span>
-            <div className="flex space-x-2">
+    <>
+      <main className="w-full flex flex-col mt-11 items-center space-y-5">
+        <section className="space-y-7 w-4/5">
+          <form className="space-y-2" onSubmit={onSubmit}>
+            <article>
+              <span className="text-sm">이메일</span>
+              <div className="flex space-x-2">
+                <input
+                  type="email"
+                  placeholder="이메일"
+                  className="input"
+                  {...register('email', {
+                    required: true,
+                    pattern: {
+                      value: REG_EMAIL,
+                      message: '이메일 형식으로 입력해주세요.',
+                    },
+                  })}
+                />
+                {/* TODO : 인증코드 요청 로직 필요 */}
+                <button type="button" className="white-btn w-2/6" onClick={getAuthVerify}>
+                  요청
+                </button>
+              </div>
+              <p className="text-xs text-[#FF0000]">
+                {errors.email?.type === 'required' && '이메일을 입력해주세요'}
+                {errors.email?.type === 'pattern' && errors.email?.message}
+              </p>
+              <p className="text-xs text-[#FF0000]">{verifyEmailReg === -1 && '이메일 형식으로 입력해주세요'}</p>
+            </article>
+            <article>
+              <span className="text-sm">인증번호</span>
+              <div className="flex space-x-2">
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="인증번호"
+                  onChange={(e) => setVerifyCode(e.target.value)}
+                />
+                {/* TODO : 인증코드 확인 로직 필요 */}
+                <button type="button" className="white-btn w-2/6" onClick={handleVerify}>
+                  인증
+                </button>
+              </div>
+              <p className={`text-xs ${verify ? 'text-blue-600' : 'text-[#FF0000]'}`}>
+                {verify ? '인증에 성공하였습니다.' : '이메일 인증을 해주세요.'}
+              </p>
+            </article>
+            <article>
+              <span className="text-sm">비밀번호</span>
+              <span className="text-xs ml-2 opacity-50">숫자, 영문, 특수문자를 포함해 8자리 이상이어야 합니다.</span>
               <input
-                type="email"
-                placeholder="이메일"
+                type="password"
+                placeholder="비밀번호"
                 className="input"
-                {...register('email', {
+                {...register('password', {
                   required: true,
                   pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    message: '이메일 형식으로 입력해주세요.',
+                    value: REG_PASSWORD,
+                    message: '숫자, 영문, 특수문자를 포함해 8자리 이상이어야 합니다.',
                   },
                 })}
               />
-              {/* TODO : 인증코드 요청 로직 필요 */}
-              <button type="button" className="white-btn w-2/6" onClick={getAuthVerify}>
-                요청
-              </button>
-            </div>
-            <p className="text-xs text-[#FF0000]">
-              {errors.email?.type === 'required' && '이메일을 입력해주세요'}
-              {errors.email?.type === 'pattern' && errors.email?.message}
-            </p>
-          </article>
-          <article>
-            <span className="text-sm">인증번호</span>
-            <div className="flex space-x-2">
-              <input
-                type="text"
-                className="input"
-                placeholder="인증번호"
-                onChange={(e) => setVerifyCode(e.target.value)}
-              />
-              {/* TODO : 인증코드 확인 로직 필요 */}
-              <button type="button" className="white-btn w-2/6" onClick={handleVerify}>
-                인증
-              </button>
-            </div>
-            <p className={`text-xs ${verify ? 'text-blue-600' : 'text-[#FF0000]'}`}>
-              {verify ? '인증에 성공하였습니다.' : '이메일 인증을 해주세요.'}
-            </p>
-          </article>
-          <article>
-            <span className="text-sm">비밀번호</span>
-            <input
-              type="password"
-              placeholder="비밀번호"
-              className="input"
-              {...register('password', {
-                required: true,
-                pattern: {
-                  value: /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
-                  message: '숫자, 영문, 특수문자를 포함해 8자리 이상이어야 합니다.',
-                },
-              })}
-            />
-            <p className="text-xs text-[#FF0000]">숫자, 영문, 특수문자를 포함해 8자리 이상이어야 합니다.</p>
-            <p className="text-xs text-[#FF0000]">
-              {errors.password?.type === 'required' && '비밀번호를 입력해주세요'}
-            </p>
-          </article>
-          <article>
-            <span className="text-sm">비밀번호 확인</span>
-            <input
-              type="password"
-              placeholder="비밀번호 확인"
-              className="input"
-              {...register('confirmPassword', {
-                required: true,
-                pattern: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
-              })}
-            />
-            <p className="text-xs text-[#FF0000]">
-              {errors.confirmPassword?.type === 'required' && '비밀번호를 재입력해주세요'}
-            </p>
-            {!isConfirm && <p className="text-xs text-[#FF0000]">비밀번호가 일치하지 않습니다.</p>}
-          </article>
-          <article className="text-sm flex flex-row pt-5">
-            <input type="checkbox" className="mx-4" onClick={() => setIsCheck(!isCheck)} />
-            <article className="opacity-60">
-              {/* TODO : 이용약관 페이지 추가 작업 */}
-              <span className="underline underline-offset-1 cursor-pointer">이용약관</span>
-              <span>을 모두 확인하였으며 이에 동의하는 것으로 간주합니다.</span>
+              <p className="text-xs text-[#FF0000]">
+                {errors.password?.type === 'required' && '비밀번호를 입력해주세요'}
+              </p>
             </article>
-          </article>
-          <button
-            type="submit"
-            className={`w-full text-base mt-4 py-1.5 bg-border-color rounded-[10px] ${
-              isCheck === true && verify === true ? '' : 'opacity-40 pointer-events-none'
-            }`}>
-            회원가입
-          </button>
-        </form>
+            <article>
+              <span className="text-sm">비밀번호 확인</span>
+              <input
+                type="password"
+                placeholder="비밀번호 확인"
+                className="input"
+                {...register('confirmPassword', {
+                  required: true,
+                  pattern: REG_PASSWORD,
+                })}
+              />
+              <p className="text-xs text-[#FF0000]">
+                {errors.confirmPassword?.type === 'required' && '비밀번호를 재입력해주세요'}
+              </p>
+              {!isConfirm && <p className="text-xs text-[#FF0000]">비밀번호가 일치하지 않습니다.</p>}
+            </article>
+            <article className="text-sm flex flex-row pt-5">
+              <input type="checkbox" className="mx-4" onClick={() => setIsCheck(!isCheck)} />
+              <article className="opacity-60">
+                {/* TODO : 이용약관 페이지 추가 작업 */}
+                <span className="underline underline-offset-1 cursor-pointer">이용약관</span>
+                <span>을 모두 확인하였으며 이에 동의하는 것으로 간주합니다.</span>
+              </article>
+            </article>
+            <button
+              type="submit"
+              className={`w-full text-base mt-4 py-1.5 bg-border-color rounded-[10px] ${
+                isCheck === true && verify === true ? '' : 'opacity-40 pointer-events-none'
+              }`}>
+              회원가입
+            </button>
+          </form>
 
-        <article className="flex flex-col justify-center items-center space-y-2 text-sm">
-          <span>
-            이미 회원이신가요?
-            <Link to="/login" className=" ml-3 text-main-red">
-              로그인
-            </Link>
-          </span>
-        </article>
-      </section>
-    </main>
+          <article className="flex flex-col justify-center items-center space-y-2 text-sm">
+            <span>
+              이미 회원이신가요?
+              <Link to="/login" className=" ml-3 text-main-red">
+                로그인
+              </Link>
+            </span>
+          </article>
+        </section>
+      </main>
+      {isModalOpen ? <AuthModal handleClose={() => setModalOpen(false)} /> : <></>}
+    </>
   );
 };
