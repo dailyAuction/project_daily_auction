4b8acfb698bb4a5bb90968f627119da505d51211
jaehoon <halmokme@gmail.com>
2023-02-14 16:55:33 +0900

    feat 상품아이템이미지 상태에따른 ui변경

--- client/src/components/_common/ProductItemImg/ProductItemImg.tsx
+++ client/src/components/_common/ProductItemImg/ProductItemImg.tsx
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router-dom';
 import { TimeRemain } from '../TimeRemain/TemiRemain';
 
 type ProductImgProps = {
@@ -6,14 +7,17 @@ type ProductImgProps = {
 };
 
 export const ProductItemImg = ({ thumbnail, statusId }: ProductImgProps) => {
+  const location = useLocation().pathname.split('/')[2];
+  const myBidWin = statusId === 1 && location === 'joinList';
+
   return (
     <div className="relative">
       <img
-        className={`${statusId === 1 ? 'opacity-60' : ''} w-[120px] h-[120px] rounded-[10px] object-cover`}
+        className={`${myBidWin ? 'opacity-60' : ''} w-[120px] h-[120px] rounded-[10px] object-cover`}
         src={thumbnail}
         alt="상품 썸네일 이미지"
       />
-      {statusId === 1 && <div className="absolute top-10 left-10 text-red-600 text-lg">낙찰!</div>}
+      {myBidWin && <div className="absolute top-10 left-10 text-red-600 text-lg">낙찰!</div>}
       <div className="absolute bottom-[95px] left-[5px]">
         {/* TODO: 남은 시간 계산하는 함수 만들어서 값 전달하기 */}
         <TimeRemain>00:23:23</TimeRemain>
