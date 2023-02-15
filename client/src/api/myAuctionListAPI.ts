5eba3ea2d44487ee32f612c94cd0cbf272ef3579
jaehoon <halmokme@gmail.com>
2023-02-15 14:42:04 +0900

    내참여경매 api 로직 분리

--- client/src/api/myAuctionListAPI.ts
+++ client/src/api/myAuctionListAPI.ts
@@ -7,11 +7,12 @@ type MyAuctionListAPIFactor = {
   size?: number;
 };
 
+const location = document.location.href.split('/')[4];
+const url = location === 'auctionList' ? 'my-auction-list' : location === 'joinList' ? 'participation-list' : '';
+
 export const myAuctionListAPI = {
   get: async ({ sort, page = 1, size = 10 }: MyAuctionListAPIFactor) => {
-    const res = await httpClient.get<ProductDetailResp[]>(
-      `/members/${sort}/my-auction-list/?page=${page}&size=${size}`
-    );
+    const res = await httpClient.get<ProductDetailResp[]>(`/members/${sort}/${url}/?page=${page}&size=${size}`);
     return res.data;
   },
 };
