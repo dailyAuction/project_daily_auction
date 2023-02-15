021d0a6e1d2cdc4cb61b46701d353ceb9b996441
jaehoon <halmokme@gmail.com>
2023-02-15 16:08:39 +0900

    feat 마감임박상품 api 로직 분리2

--- client/src/api/closingProductAPI.ts
+++ client/src/api/closingProductAPI.ts
@@ -3,7 +3,7 @@ import { httpClient } from '../utils/httpClient';
 
 export const closingProductAPI = {
   get: async () => {
-    const res = await httpClient.get<ProductDetailResp[]>('/dddimminent-item');
+    const res = await httpClient.get<ProductDetailResp[]>('/imminent-item');
     return res.data;
   },
 };
