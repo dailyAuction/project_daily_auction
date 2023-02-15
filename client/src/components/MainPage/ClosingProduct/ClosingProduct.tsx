cd745c4e33b47d5a08a9e2ff1d893467048ca1b9
jaehoon <halmokme@gmail.com>
2023-02-15 16:07:56 +0900

    feat 마감임박상품 쿼리옵션 변경

--- client/src/components/MainPage/ClosingProduct/ClosingProduct.tsx
+++ client/src/components/MainPage/ClosingProduct/ClosingProduct.tsx
@@ -1,26 +1,15 @@
-import axios from 'axios';
 import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import { ProductItemImg } from '../../_common/ProductItemImg/ProductItemImg';
-import { ProductDetailResp } from '../../../types/product.type';
-
-const getClosingProduct = async () => {
-  const { data } = await axios.get<ProductDetailResp[]>(`${process.env.REACT_APP_URL}/imminent-item`);
-  return data;
-};
+import { closingProductAPI } from '../../../api/closingProductAPI';
 
 export const ClosingProduct = () => {
-  const {
-    data: closingProduct,
-    isLoading,
-    isError,
-  } = useQuery<ProductDetailResp[], Error>('closingProduct', getClosingProduct, {
-    retry: 0,
-    onError: (e) => console.log(e.message),
+  const { isLoading, error, data } = useQuery('myAuction', () => closingProductAPI.get(), {
+    // 현재 데이터가 없어서 쿼리를 1초단위로 보내는만큼 매초 에러가 발생해서 우선 주석처리 해두었습니다.
+    // refetchOnMount: true,
+    // refetchOnWindowFocus: true,
+    // refetchInterval: 1000,
   });
-
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>마감 임박 상품이 없습니다.</div>;
 
   return (
     <div className=" w-full my-4 px-2">
@@ -37,7 +26,9 @@ export const ClosingProduct = () => {
         </svg>
       </div>
       <div className="flex gap-2 overflow-x-auto scrollbar-hide w-full">
-        {closingProduct?.map((el) => {
+        {isLoading && <div>Loading...</div>}
+        {error && <div>마감 임박 상품이 없습니다.</div>}
+        {data?.map((el) => {
           return (
             <Link key={el.boardId} to={`/detail/${el.boardId}`}>
               <div className="flex flex-col ml-2 min-w-[120px] w-[120px]">
