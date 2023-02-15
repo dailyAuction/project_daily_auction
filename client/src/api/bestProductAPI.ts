be6d31c82dd764bdbc954cccff5f475a73170689
jaehoon <halmokme@gmail.com>
2023-02-15 16:26:24 +0900

    chore 기타 철자 수정2

--- client/src/api/bestProductAPI.ts
+++ client/src/api/bestProductAPI.ts
@@ -1,12 +1,12 @@
 import { ProductDetailResp } from '../types/product.type';
 import { httpClient } from '../utils/httpClient';
 
-type bestProductAPIFactor = {
+type BestProductAPIFactor = {
   path: string;
 };
 
 export const bestProductAPI = {
-  get: async ({ path }: bestProductAPIFactor) => {
+  get: async ({ path }: BestProductAPIFactor) => {
     const res = await httpClient.get<ProductDetailResp[]>(`/${path}`);
     return res.data;
   },
