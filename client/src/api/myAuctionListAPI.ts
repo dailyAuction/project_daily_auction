9b3f22fa6faf6dc3741541f8725652b2f284a50b
jaehoon <halmokme@gmail.com>
2023-02-15 14:47:21 +0900

    feat api로직분리 코드수정

--- client/src/api/myAuctionListAPI.ts
+++ client/src/api/myAuctionListAPI.ts
@@ -3,16 +3,14 @@ import { httpClient } from '../utils/httpClient';
 
 type MyAuctionListAPIFactor = {
   sort: string;
+  path: string;
   page?: number;
   size?: number;
 };
 
-const location = document.location.href.split('/')[4];
-const url = location === 'auctionList' ? 'my-auction-list' : location === 'joinList' ? 'participation-list' : '';
-
 export const myAuctionListAPI = {
-  get: async ({ sort, page = 1, size = 10 }: MyAuctionListAPIFactor) => {
-    const res = await httpClient.get<ProductDetailResp[]>(`/members/${sort}/${url}/?page=${page}&size=${size}`);
+  get: async ({ sort, path, page = 1, size = 10 }: MyAuctionListAPIFactor) => {
+    const res = await httpClient.get<ProductDetailResp[]>(`/members/${sort}/${path}/?page=${page}&size=${size}`);
     return res.data;
   },
 };
