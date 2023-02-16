bc7631c91ff81d7f92c8d72804b971ea4ab60d59
Hyejung Park <hyejj19@naver.com>
2023-02-16 16:45:35 +0900

    refactor : modal 컴포넌트 별도 파일로 분리

--- client/src/components/DetailPage/BidInformation/BidInformation.tsx
+++ client/src/components/DetailPage/BidInformation/BidInformation.tsx
@@ -4,65 +4,18 @@ import { useQuery } from 'react-query';
 import { useRecoilValue } from 'recoil';
 import { userInfoAtom } from '../../../atoms/user';
 import { useHandleIsLogin } from '../../../hooks/useHandleIsLogin';
-import { useBidInformation, useBidInformationModal } from './useBidInformation';
+import { useBidInformation } from './useBidInformation';
 
 import { productDetailAPI } from '../../../api/boardsAPI';
-import { blockInvalidChar } from '../../../utils/blockInvalidChar';
+import { ProductDetailRealtimeResp } from '../../../types/product.type';
+import { BidModal } from './BidModal';
 
 type BidInformationProps = {
-  currentPrice: string;
+  reatTimeData: ProductDetailRealtimeResp;
   sendBid: (price: string) => void;
 };
 
-type BidModalProps = BidInformationProps & {
-  handleClose: () => void;
-};
-
-// 입찰 모달
-const BidModal = ({ handleClose, currentPrice, sendBid }: BidModalProps) => {
-  const [bidValue, setBidValue] = useState('');
-  const [validationMsg, setValidationMsg] = useState('');
-
-  const { handleClickBid, handleChange } = useBidInformationModal({ bidValue, setBidValue, setValidationMsg });
-
-  const { coin: myCoin } = useRecoilValue(userInfoAtom);
-
-  return (
-    <section className="bg-modal z-10">
-      <div className="modal-container">
-        <article className="flex flex-col space-y-1">
-          <span>현재 경매가</span>
-          <span className="text-xl font-bold text-main-orange">{currentPrice} coin</span>
-        </article>
-        <article>
-          <input
-            placeholder="입찰가를 입력해주세요"
-            onChange={handleChange}
-            value={bidValue}
-            className="input"
-            type="number"
-            onKeyDown={blockInvalidChar}
-          />
-          <span className="text-xs text-main-red">{validationMsg}</span>
-        </article>
-        <article className="flex self-end space-x-1 items-center">
-          <span className="text-sm">현재 내 코인 : </span>
-          <span className="text-bold text-main-orange h-3">{myCoin} coin</span>
-        </article>
-        <article className="flex justify-between pt-3">
-          <button type="submit" className="red-btn" onClick={() => handleClickBid(currentPrice, sendBid)}>
-            입찰
-          </button>
-          <button type="submit" className="white-btn" onClick={handleClose}>
-            취소
-          </button>
-        </article>
-      </div>
-    </section>
-  );
-};
-
-export const BidInformation = ({ currentPrice, sendBid }: BidInformationProps) => {
+export const BidInformation = ({ reatTimeData, sendBid }: BidInformationProps) => {
   const [isModalOpen, setModalOpen] = useState(false);
 
   const handleOpenModal = () => {
@@ -86,7 +39,11 @@ export const BidInformation = ({ currentPrice, sendBid }: BidInformationProps) =
     }
   );
 
-  const { bidCount, statusId, startingPrice, myPrice = null, authorId } = data || {};
+  // 일반 경매 정보 데이터
+  const { statusId, startingPrice, myPrice = null, authorId } = data || {};
+
+  // 실시간 경매 정보 데이터
+  const { price: currentPrice, bidCount } = reatTimeData;
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>에러가 발생하였습니다.</div>;
