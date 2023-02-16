08a1651589b5a2caad8b055a1f554f27cc49a098
raon9401 <raon9401@naver.com>
2023-02-16 13:25:20 +0900

    feat : 인증번호 요청 이메일확인 결과에 따른 텍스트 변경

--- client/src/components/SignUpPage/SignUpModal/AuthModal.tsx
+++ client/src/components/SignUpPage/SignUpModal/AuthModal.tsx
@@ -2,14 +2,17 @@ type CheckModalProps = {
   handleClose: () => void;
 };
 
-export const AuthModal = ({ handleClose }: CheckModalProps) => {
+export const AuthModal = (verifyError, { handleClose }: CheckModalProps) => {
   return (
     <section className="bg-modal">
       <div className="modal-container justify-around items-center py-5">
-        <span className="text-lg font-bold">이메일 인증 요청</span>
+        <span className="text-lg font-bold">{verifyError ? '가입된 이메일' : '이메일 인증 요청'}</span>
         <article className="flex flex-col items-center text-base">
-          <span>입력하신 이메일로 인증 코드를 보냈습니다.</span>
-          <span>이메일을 확인해주세요.</span>
+          <span>
+            {verifyError
+              ? '해당 이메일은 이미 가입되어 있습니다. 로그인을 해주세요.'
+              : '입력하신 이메일로 인증 코드를 보냈습니다. 이메일을 확인해주세요.'}
+          </span>
         </article>
         <article className="w-full flex justify-around font-bold">
           <button
