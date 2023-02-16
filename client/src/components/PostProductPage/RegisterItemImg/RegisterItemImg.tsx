826bf9a6ed53971c4cffae6db802c2be30a09170
jaehoon <halmokme@gmail.com>
2023-02-16 16:03:55 +0900

    feat 이미지 개수 제한

--- client/src/components/PostProductPage/RegisterItemImg/RegisterItemImg.tsx
+++ client/src/components/PostProductPage/RegisterItemImg/RegisterItemImg.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 
 export const RegisterItemImg = ({ myImage, setMyImage }) => {
-  const addImage = (e) => {
+  const handleAddImage = (e) => {
     const nowSelectImageList = e.target.files;
     const nowImgURLList = [...myImage];
     const nowImageUrl = URL.createObjectURL(nowSelectImageList[0]);
-    nowImgURLList.push(nowImageUrl);
-    setMyImage(nowImgURLList);
+    if (myImage.length > 4) alert('이미지는 5개까지 등록 가능합니다');
+    else {
+      nowImgURLList.push(nowImageUrl);
+      setMyImage(nowImgURLList);
+    }
   };
 
   return (
     <section className="base-layout overflow-x-auto scrollbar-hide flex items-start justify-center h-1/5 border-b-2">
       <div className="flex px-2">
-        <label onChange={addImage} htmlFor="input-file">
+        <label onChange={handleAddImage} htmlFor="input-file">
           <div className="w-24 h-24 bg-white border-2 border-light-gray rounded-[10px] p-1.5 mr-3">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -27,16 +30,15 @@ export const RegisterItemImg = ({ myImage, setMyImage }) => {
           <input type="file" id="input-file" multiple accept="image/*" style={{ display: 'none' }} />
         </label>
         <div className="flex justify-start align-middle">
-          {myImage.length <= 5 &&
-            myImage.map((x) => {
-              return (
-                <div
-                  key={x}
-                  className="flex justify-center align-middle w-24 h-24 bg-white border-2 border-light-gray rounded-[10px] mr-3">
-                  <img className="rounded-[10px]" src={x} alt="img" />
-                </div>
-              );
-            })}
+          {myImage.map((x) => {
+            return (
+              <div
+                key={x}
+                className="flex justify-center align-middle w-24 h-24 bg-white border-2 border-light-gray rounded-[10px] mr-3">
+                <img className="rounded-[10px]" src={x} alt="img" />
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
