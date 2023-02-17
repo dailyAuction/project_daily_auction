74632cc10ebe461da85b3c5190751df177eae658
raon9401 <raon9401@naver.com>
2023-02-17 13:43:02 +0900

    chore : API 통합(로그인, 회원가입)

--- client/src/api/signupAPI.ts
+++ client/src/api/signupAPI.ts
@@ -3,9 +3,21 @@ import { httpClient } from '../utils/httpClient';
 
 type SignUpData = MemberAuthData;
 
+type VerifyCodeFactor = {
+  verifyCode: string;
+};
+
 export const signupAPI = {
-  url: '/members/signup',
+  url: '/members',
+
+  // 회원가입
   post: async (data: SignUpData) => {
     await httpClient.post<SignUpData>(signupAPI.url, data);
+  },
+
+  // 이메일 인증 요청
+  postVerified: async (email: string) => {
+    const res = await httpClient.post<VerifyCodeFactor>(`${signupAPI.url}/verification`, email);
+    return res.data;
   },
 };
