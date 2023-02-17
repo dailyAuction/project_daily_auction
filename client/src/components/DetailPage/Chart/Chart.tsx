32e55e158a244f618d89b532af4368cc3a632936
casava840 <107945688+casava840@users.noreply.github.com>
2023-02-17 16:08:26 +0900

    Delete client directory

--- client/src/components/DetailPage/Chart/Chart.tsx
+++
@@ -1,7 +0,0 @@
-export const Chart = () => {
-  return (
-    <section className="flex h-24 w-full justify-center items-center border">
-      <span>Chart.js 를 사용하여 구현 예정</span>
-    </section>
-  );
-};
--- client/src/components/NotificationPage/Notification/useNotification.ts
+++
@@ -1,8 +0,0 @@
-export const useNotification = () => {
-  const handleDelete = (id) => {
-    // TODO: 삭제 로직 구현
-    console.log('delete');
-  };
-
-  return { handleDelete };
-};
