184623a4ae041604182e3c07fd9017f14c991f75
woong-sung <cjswo138@gmail.com>
2023-02-17 22:13:21 +0900

    feat: 게시글 웹소켓 입장메시지 추가

--- server/src/main/java/com/project/dailyAuction/webSocket/BidController.java
+++ server/src/main/java/com/project/dailyAuction/webSocket/BidController.java
@@ -1,19 +1,22 @@
 package com.project.dailyAuction.webSocket;
 
+import com.project.dailyAuction.board.Dto.BoardDto;
 import com.project.dailyAuction.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class BidController {
     private final BoardService boardService;
     private final SimpMessageSendingOperations simpMessageSendingOperations;
 
     @MessageMapping("/bid")
-    public void message(Message message) {
+    public void bidMessage(Message.Bid message) {
         long boardId = message.getBoardId();
         String token = message.getBidderToken();
         int newPrice = message.getPrice();
@@ -23,11 +26,50 @@ public class BidController {
         int bidCount = boardService.getBidCountInRedis(boardId);
         String history = boardService.getHistoryInRedis(boardId);
 
-        Message response = Message.builder()
+        Message.Response response = Message.Response.builder()
                 .boardId(boardId)
                 .bidCount(bidCount)
                 .history(history)
                 .price(message.getPrice())
+                .build();
+        simpMessageSendingOperations.convertAndSend("/sub/board-id/" + message.getBoardId(), response);
+    }
+
+    @MessageMapping("/init")
+    public void initMessage(Message.Init message) {
+        long boardId = message.getBoardId();
+        String token = message.getBidderToken();
+
+        int bidCount = boardService.getBidCountInRedis(boardId);
+
+        String history = boardService.getHistoryInRedis(boardId);
+        int currentPrice = boardService.getPriceInRedis(boardId);
+        long bidderId = boardService.getBidderInRedis(boardId);
+        int viewCount = boardService.addViewCntToRedis(boardId);
+        int myPrice = boardService.findMyPrice(token, boardId);
+
+        BoardDto.Response dto = boardService.getDetailPage(token, boardId, currentPrice, viewCount, bidCount, bidderId, history);
+
+
+        Message.InitResponse response = Message.InitResponse.builder()
+                .boardId(boardId)
+                .authorId(dto.getSellerId())
+                .bidCount(bidCount)
+                .bidderId(bidderId)
+                .title(dto.getTitle())
+                .description(dto.getDescription())
+                .image(dto.getImage())
+                .thumbnail(dto.getThumbnail())
+                .statusId(dto.getStatusId())
+                .categoryId(dto.getCategoryId())
+                .viewCount(viewCount)
+                .bidCount(bidCount)
+                .startingPrice(dto.getStartingPrice())
+                .currentPrice(dto.getCurrentPrice())
+                .createdAt(dto.getCreatedAt())
+                .finishedAt(dto.getFinishedAt())
+                .history(dto.getHistory())
+                .myPrice(myPrice)
                 .build();
         simpMessageSendingOperations.convertAndSend("/sub/board-id/" + message.getBoardId(), response);
     }
--- server/src/main/java/com/project/dailyAuction/webSocket/Message.java
+++ server/src/main/java/com/project/dailyAuction/webSocket/Message.java
@@ -5,14 +5,60 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+import java.time.LocalDateTime;
+
+
 public class Message {
-    private long boardId;
-    private int price;
-    private String bidderToken;
-    private int bidCount;
-    private String history;
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Bid{
+        private long boardId;
+        private int price;
+        private String bidderToken;
+    }
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Init{
+        private long boardId;
+        private String bidderToken;
+    }
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Response{
+        private long boardId;
+        private int price;
+        private String bidderToken;
+        private int bidCount;
+        private String history;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class InitResponse {
+        private long boardId;
+        private long authorId;
+        private long bidderId;
+        private String title;
+        private String description;
+        private String image;
+        private String thumbnail;
+        private long statusId;
+        private long categoryId;
+        private int viewCount;
+        private int bidCount;
+        private int startingPrice;
+        private int currentPrice;
+        private LocalDateTime createdAt;
+        private LocalDateTime finishedAt;
+        private Integer[] history;
+        private int myPrice;
+    }
 }
