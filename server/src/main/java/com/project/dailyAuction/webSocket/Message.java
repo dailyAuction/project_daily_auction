0c24248f698eb855545ae6a5e5162fa1f2b7f880
woong-sung <cjswo138@gmail.com>
2023-02-20 14:10:15 +0900

    feat: 응답 데이터 수정

--- server/src/main/java/com/project/dailyAuction/webSocket/Message.java
+++ server/src/main/java/com/project/dailyAuction/webSocket/Message.java
@@ -5,9 +5,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
-
 public class Message {
     @Getter
     @Builder
@@ -44,20 +41,8 @@ public class Message {
     @NoArgsConstructor
     public static class InitResponse {
         private long boardId;
-        private long authorId;
-        private long bidderId;
-        private String title;
-        private String description;
-        private String image;
-        private String thumbnail;
-        private long statusId;
-        private long categoryId;
-        private int viewCount;
         private int bidCount;
-        private int startingPrice;
         private int currentPrice;
-        private LocalDateTime createdAt;
-        private LocalDateTime finishedAt;
         private Integer[] history;
         private int myPrice;
     }
