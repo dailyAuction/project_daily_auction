978cf38d04e112a4b34e1e297ed76cd6df5dd168
Hyejung Park <hyejj19@naver.com>
2023-02-21 17:28:50 +0900

    feat : 웹소켓 요청로직 구현
    * 입찰을 위해 publish 요청을 수행합니다.

--- client/src/hooks/useWebsocket.ts
+++ client/src/hooks/useWebsocket.ts
@@ -1,36 +1,38 @@
 import { Client } from '@stomp/stompjs';
-import { useEffect, useState } from 'react';
-
-const TEMP_TOKEN =
-  'Bearer eyJhbGciOiJIUzUxMiJ9.eyJlbWFpbCI6InF3ZTVAZ21haWwuY29tIiwibWVtYmVySWQiOjcsInN1YiI6InF3ZTVAZ21haWwuY29tIiwiaWF0IjoxNjc2OTUyNzkzLCJleHAiOjE2NzY5NTQ1OTN9.gqXLYnDPjGjC7o-qYimuaHsJqisGijoOG8kFoN9D916OsAHNeNFHHg6u-Zc0Z6YIsVfgda1Uix2czgu_WqIgAQ';
+import { useEffect, useRef, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { accessTokenAtom } from '../atoms/token';
+import { ProductDetailRealtimeResp } from '../types/product.type';
 
 export const useWebsocket = (subEndpoint: string) => {
-  // TODO: 타입 선언 수정하기
-  const [response, setResponse] = useState<any>({});
-  const boardId = subEndpoint.split('/').at(-1);
+  const [response, setResponse] = useState<Partial<ProductDetailRealtimeResp>>({});
+  const boardId = +subEndpoint.split('/').at(-1);
+  const client = useRef<Client | null>(null);
+  const accessToken = useRecoilValue(accessTokenAtom);
 
   // 웹소켓 연결을 위한 stomp 클라이언트 생성
-  const client = new Client({
-    brokerURL: `ws:${process.env.REACT_APP_WEBSOCKET_URL}/ws`,
-    debug: (str) => console.log(str),
-    // 자동 재연결을 위한 옵션
-    reconnectDelay: -1,
-    // server와 client의 연결상태 확인을 위한 auto send 기능
-    heartbeatIncoming: 10000,
-    heartbeatOutgoing: 10000,
-  });
-
   useEffect(() => {
     // 구독 주소로 연결, 받은 메시지를 상태값으로 저장
-    client.activate();
+    client.current = new Client({
+      brokerURL: `ws:${process.env.REACT_APP_WEBSOCKET_URL}/ws`,
+      debug: (str) => console.log(str),
+      // 자동 재연결을 위한 옵션
+      reconnectDelay: 1,
+      // server와 client의 연결상태 확인을 위한 auto send 기능
+      heartbeatIncoming: 10000,
+      heartbeatOutgoing: 10000,
+    });
 
-    client.onConnect = (frame) => {
+    // 웹소켓 연결
+    client.current.onConnect = (frame) => {
       console.log(`connected: ${frame}`);
 
       // 받아온 데이터를 응답값에 저장한다.
-      client.subscribe(subEndpoint, (res) => setResponse(JSON.parse(res.body)));
+      client.current.subscribe(subEndpoint, (res) => {
+        setResponse(JSON.parse(res.body));
+      });
 
-      client.publish({
+      client.current.publish({
         destination: '/pub/init',
         body: JSON.stringify({
           boardId,
@@ -39,30 +41,34 @@ export const useWebsocket = (subEndpoint: string) => {
       });
     };
 
+    // FIXME: 서버에서 전송되는 에러 로그 찍을 수 있도록 수정
+    client.current.onStompError = (frame) => {
+      console.log('Broker reported error: ' + frame.headers.message);
+      console.log('Additional details: ' + frame.body);
+      console.log(frame);
+    };
+
+    client.current.activate();
     // 컴포넌트 언마운트시 연결을 해제
     return () => {
-      client.deactivate();
+      client.current.deactivate();
     };
   }, []);
 
-  const sendBid = (price: string) => {
+  // 입찰 요청 함수
+  const sendBid = (price: number) => {
     const destination = '/pub/bid';
 
-    client.activate();
+    const body = JSON.stringify({
+      boardId,
+      price,
+      bidderToken: accessToken,
+    });
 
-    client.onConnect = () => {
-      console.log('입찰!');
-      // FIXME: 입찰 요청 로직 완성하기
-      client.publish({
-        destination,
-        headers: { receipt: 'send-bid-receipt' },
-        body: JSON.stringify({
-          boardId,
-          price,
-          bidderToken: TEMP_TOKEN,
-        }),
-      });
-    };
+    client.current.publish({
+      destination,
+      body,
+    });
   };
 
   return { response, sendBid };
