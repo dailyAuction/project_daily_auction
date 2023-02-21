967657d3b2d471307bed162c760bf332652f31d8
jaehoon <halmokme@gmail.com>
2023-02-21 13:55:15 +0900

    design 상품이미지 남은시간 표시부분 너비 변경

--- client/src/components/_common/TimeRemain/TemiRemain.tsx
+++ client/src/components/_common/TimeRemain/TemiRemain.tsx
@@ -1,10 +1,23 @@
+// export const TimeRemain = ({ children }) => {
+//   const oneHourLeft = children.slice(0, 2) === '00';
+
+//   return (
+//     <button
+//       type="button"
+//       className={`rounded-[10px] px-2.5 shadow-lg w-[70px] h-[18px] text-xs ${
+//         oneHourLeft ? 'bg-[#E4E4E4] text-main-red' : 'bg-black text-white opacity-40'
+//       } `}>
+//       {children}
+//     </button>
+//   );
+// };
 export const TimeRemain = ({ children }) => {
   const oneHourLeft = children.slice(0, 2) === '00';
 
   return (
     <button
       type="button"
-      className={`rounded-[10px] px-2.5 shadow-lg w-[70px] h-[18px] text-xs ${
+      className={`rounded-[10px] px-2.5 shadow-lg w-full h-[18px] text-xs ${
         oneHourLeft ? 'bg-[#E4E4E4] text-main-red' : 'bg-black text-white opacity-40'
       } `}>
       {children}
