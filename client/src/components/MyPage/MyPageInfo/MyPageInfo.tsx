eb6ef5ae34267813ba14acca24a278253283f578
raon9401 <raon9401@naver.com>
2023-02-21 15:46:26 +0900

    feat : 로그인에 따른 마이 페이지 정보 수정

--- client/src/components/MyPage/MyPageInfo/MyPageInfo.tsx
+++ client/src/components/MyPage/MyPageInfo/MyPageInfo.tsx
@@ -1,39 +1,23 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { loginStateAtom } from '../../../atoms/user';
-
-type LeaveModalProps = {
-  handleClose: () => void;
-};
-
-const LeaveModal = ({ handleClose }: LeaveModalProps) => {
-  return (
-    <section className="bg-modal">
-      <div className="modal-container justify-around items-center py-5">
-        <span className="text-lg font-bold">회원탈퇴</span>
-        <article className="flex flex-col items-center text-base">
-          <p>남은 예치금은 탈퇴시 소멸됩니다.</p>
-          <p>
-            정말 <span className="text-main-red font-bold">탈퇴</span>하시겠습니까?
-          </p>
-        </article>
-        <article className="w-full flex justify-around font-bold">
-          <button type="submit" className="white-btn">
-            예
-          </button>
-          <button type="submit" className="red-btn" onClick={handleClose}>
-            아니오
-          </button>
-        </article>
-      </div>
-    </section>
-  );
-};
+import { useRecoilValue, useResetRecoilState } from 'recoil';
+import { accessTokenAtom, refreshTokenAtom } from '../../../atoms/token';
+import { userInfoAtom } from '../../../atoms/user';
+import { SignOutModal } from './SignOutModal';
 
 export const MyPageInfo = () => {
   const [isModalOpen, setModalOpen] = useState(false);
-  const [, setLoginState] = useRecoilState(loginStateAtom);
+  const { email, coin } = useRecoilValue(userInfoAtom);
+
+  const resetAccessToken = useResetRecoilState(accessTokenAtom);
+  const resetRefreshToken = useResetRecoilState(refreshTokenAtom);
+  const resetUserInfoAtom = useResetRecoilState(userInfoAtom);
+
+  const handleLogout = () => {
+    resetAccessToken();
+    resetRefreshToken();
+    resetUserInfoAtom();
+  };
 
   const myAuctionBtn = 'w-full bg-background-mobile p-6';
 
@@ -43,9 +27,8 @@ export const MyPageInfo = () => {
         <article className="flex flex-row h-full justify-between items-center">
           <div className="font-medium text-sm ">
             <span>현재 로그인 : </span>
-            <span className="font-bold">email@gmail.com</span>
+            <span className="font-bold">{email}</span>
           </div>
-          {/* TODO : 비밀번호 변경 페이지로 LINK */}
           <Link to="/editPassword">
             <span className="text-xs font-light">비밀번호 변경 &gt;</span>
           </Link>
@@ -54,9 +37,7 @@ export const MyPageInfo = () => {
       <section className="w-full px-[10px] py-3 bg-white">
         <span className="text-base font-bold">내 코인</span>
         <article className="flex flex-row justify-between my-2">
-          {/* TODO : 데이터에 따라 코인 수 변경 */}
-          <span className="text-xl font-bold text-main-orange">10,000 coin</span>
-          {/* TODO : 모달 추가하기 */}
+          <span className="text-xl font-bold text-main-orange">{coin} coin</span>
           <Link to="/charge">
             <button type={'button'} className="red-btn text-sm font-bold">
               충전하기
@@ -64,7 +45,6 @@ export const MyPageInfo = () => {
           </Link>
         </article>
         <article className="w-full flex flex-row mt-4">
-          {/* TODO : 각 페이지로 링크 작업 */}
           <button className={`${myAuctionBtn} border-r-2 border-light-gray`} type="button">
             <Link to="/my/auctionList">내가 등록한 경매</Link>
           </button>
@@ -75,12 +55,11 @@ export const MyPageInfo = () => {
       </section>
       <section className="w-full px-[10px] py-3 bg-white">
         {/* TODO : 모달 추가하기 */}
-        <button type="button" onClick={() => setLoginState(false)}>
+        <button type="button" onClick={handleLogout}>
           로그아웃
         </button>
       </section>
       <section className="w-full px-[10px] py-3 bg-white">
-        {/* TODO : 모달 추가하기 */}
         <button
           type="button"
           onClick={() => {
@@ -88,7 +67,7 @@ export const MyPageInfo = () => {
           }}>
           회원 탈퇴
         </button>
-        {isModalOpen ? <LeaveModal handleClose={() => setModalOpen(false)} /> : <></>}
+        {isModalOpen ? <SignOutModal handleClose={() => setModalOpen(false)} /> : <></>}
       </section>
     </main>
   );
