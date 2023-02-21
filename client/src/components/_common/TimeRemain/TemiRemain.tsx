bf06027a742e0ba9798efcac4394f3fd53209382
jaehoon <halmokme@gmail.com>
2023-02-21 14:05:43 +0900

    chore 주석제거

--- client/src/components/_common/TimeRemain/TemiRemain.tsx
+++ client/src/components/_common/TimeRemain/TemiRemain.tsx
@@ -1,16 +1,3 @@
-// export const TimeRemain = ({ children }) => {
-//   const oneHourLeft = children.slice(0, 2) === '00';
-
-//   return (
-//     <button
-//       type="button"
-//       className={`rounded-[10px] px-2.5 shadow-lg w-[70px] h-[18px] text-xs ${
-//         oneHourLeft ? 'bg-[#E4E4E4] text-main-red' : 'bg-black text-white opacity-40'
-//       } `}>
-//       {children}
-//     </button>
-//   );
-// };
 export const TimeRemain = ({ children }) => {
   const oneHourLeft = children.slice(0, 2) === '00';
 
