fdf3316db5424cd2cbf713f5a0f9edb79b9205cd
Hyejung Park <hyejj19@naver.com>
2023-02-21 17:27:32 +0900

    feat : 상품 상태 컴포넌트 뷰-로직 분리, 현재 상태 메시지 리턴 함수 생성

--- client/src/components/DetailPage/AuctionStatus/AuctionStatus.tsx
+++ client/src/components/DetailPage/AuctionStatus/AuctionStatus.tsx
@@ -1,13 +1,9 @@
 import { ProductStatus } from '../../_common/ProductStatus/ProductStatus';
-import { AUCTION_STATUS } from '../../../constants/constants';
 import { useGetTimeRemain } from '../../../hooks/useGetTimeRemain';
+import { useAuctionStatus } from './useAuctionStatus';
 
-type AuctionStatusProps = {
-  finishedAt: string;
-  statusId: number;
-};
-
-export const AuctionStatus = ({ finishedAt, statusId }: AuctionStatusProps) => {
+export const AuctionStatus = () => {
+  const { finishedAt, checkStatus } = useAuctionStatus();
   // 종료 시간을 전달하면, 현재 시간으로 남은 시간을 시:분:초 형태의 string으로 리턴
   const { timeRemain } = useGetTimeRemain(finishedAt);
 
@@ -17,8 +13,8 @@ export const AuctionStatus = ({ finishedAt, statusId }: AuctionStatusProps) => {
         <span>남은 시간</span>
         <span className="text-main-orange">{timeRemain}</span>
       </article>
-      {/* TODO: 로그인 여부, 판매자 / 입찰자 여부 판단하여 조건부 렌더링 */}
-      <ProductStatus>{AUCTION_STATUS[statusId]}</ProductStatus>
+
+      <ProductStatus>{checkStatus()}</ProductStatus>
     </section>
   );
 };
