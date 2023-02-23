389b929611f83255553edb82a7f730f60ee5c60c
조 성웅 <cjswo138@gmail.com>
2023-02-23 18:23:15 +0900

    test: destination 변경 start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd /home/project_daily_auction/server/build/libs
+cd /home/ec2-user/build
 sudo nohup java -jar project_daily_auction-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
