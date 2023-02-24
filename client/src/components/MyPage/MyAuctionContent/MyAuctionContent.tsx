4977ec71f954142356068ed5337c4ce796caa81d
jaehoon <halmokme@gmail.com>
2023-02-24 15:10:24 +0900

    feat 내경매 statusId값 변경

--- client/src/components/MyPage/MyAuctionContent/MyAuctionContent.tsx
+++ client/src/components/MyPage/MyAuctionContent/MyAuctionContent.tsx
@@ -1,7 +1,7 @@
 import { ProductItem } from '../../_common/ProductItem/ProductItem';
 
 export const MyAuctionContent = ({ details, status }) => {
-  const viewDataFilter = details?.items.filter((el) => el.statusId === status);
+  const viewDataFilter = details?.items.filter((el) => el.statusId === status + 1);
 
   return (
     <div className="w-full px-2.5 h-4/5 overflow-x-auto scrollbar-hide flex flex-col gap-2">
