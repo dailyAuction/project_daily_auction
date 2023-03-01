12bac02ee4b2ad26e254085e4f693db9db404bd6
raon9401 <raon9401@naver.com>
2023-03-01 16:59:25 +0900

    feat : 카테고리 상품 페이지 로직 구현

--- client/src/components/_common/CategoryList/CategoryList.tsx
+++ client/src/components/_common/CategoryList/CategoryList.tsx
@@ -1,6 +1,18 @@
+import { SetStateAction } from 'react';
 import { CATEGORIE_SORT_STATUS } from '../../../constants/constants';
 
-export const CategoryList = ({ sortState, setSortState }) => {
+type CategoryFactor = {
+  sortState: number;
+  setSortState: React.Dispatch<SetStateAction<number>>;
+  setIsClick: React.Dispatch<SetStateAction<boolean>>;
+};
+
+export const CategoryList = ({ sortState, setSortState, setIsClick }: CategoryFactor) => {
+  const handleClickCatogry = (index: number) => {
+    setSortState(index);
+    setIsClick(true);
+  };
+
   return (
     <nav className="h-full p-2 pb-4 overflow-x-scroll scrollbar-hide">
       <section className="w-max space-x-3">
@@ -9,7 +21,7 @@ export const CategoryList = ({ sortState, setSortState }) => {
             key={item}
             type="button"
             className={`category-btn ${sortState === index && 'bg-main-red text-white pointer-events-none'}`}
-            onClick={() => setSortState(index)}>
+            onClick={() => handleClickCatogry(index)}>
             {item}
           </button>
         ))}
