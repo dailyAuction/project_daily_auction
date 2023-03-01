656c122ff854e415dac83f795d15852e3108a660
Hyejung Park <hyejj19@naver.com>
2023-03-01 17:20:54 +0900

    fix : 키워드 검색 결과 보이도록 수정

--- client/src/api/searchAPI.ts
+++ client/src/api/searchAPI.ts
@@ -1,5 +1,5 @@
-import { ProductDetailResp } from '../types/product.type';
 import { httpClient } from '../utils/httpClient';
+import { ProductListResp } from '../types/product.type';
 
 type SearchAPIFactor = {
   categoryId: string;
@@ -10,8 +10,8 @@ type SearchAPIFactor = {
 
 export const searchAPI = {
   get: async ({ categoryId, keyword = '', page = 1, size = 10 }: SearchAPIFactor) => {
-    const res = await httpClient.get<ProductDetailResp[]>(
-      `/${categoryId}/search?page=${page}&size=${size}&keyword-${encodeURI(keyword)}`
+    const res = await httpClient.get<ProductListResp>(
+      `/${categoryId}/search/?page=${page}&size=${size}&keyword=${encodeURI(keyword)}`
     );
     return res.data;
   },
