eeb83ce1323935b87179418b8cdc8e408a18945b
Hyejung Park <hyejj19@naver.com>
2023-03-02 18:05:13 +0900

    feat : 알림 컴포넌트 SSE 응답 데이터와 연결

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -2,7 +2,7 @@ export const CATEGORIES = ['ALL', '디지털 / 가전', '가구 / 생활', '의�
 
 export const AUCTION_STATUS = ['진행중', '낙찰', '마감'];
 
-export const NOTIFICATION_STATUS = ['낙찰', '낙찰', '유찰', '상회 입찰', '마감 임박'];
+export const NOTIFICATION_STATUS = ['', '낙찰', '낙찰', '유찰', '상회 입찰', '마감 임박'];
 
 export const NOTIFICATION_STATUS_MSG = [
   '님이 낙찰되셨습니다.',
@@ -19,3 +19,5 @@ export const CATEGORIE_SORT_STATUS = ['마감 임박', 'TOP 입찰 횟수', 'TOP
 export const REG_EMAIL = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 export const REG_PASSWORD = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 export const REG_KOREA = /[ㄱ-ㅎㅏ-ㅣ가-힣]+/g;
+
+export const NOTIFICATION_KEY = 'notifications';
