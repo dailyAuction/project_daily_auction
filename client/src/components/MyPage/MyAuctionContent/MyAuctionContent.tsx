2334d66a3dadb707c770da9012e9eb904880857b
jaehoon <halmokme@gmail.com>
2023-03-03 16:05:33 +0900

    feat 내등록경매페이지 무한스크롤

--- client/src/components/MyPage/MyAuctionContent/MyAuctionContent.tsx
+++ client/src/components/MyPage/MyAuctionContent/MyAuctionContent.tsx
@@ -1,12 +1,19 @@
 import { ProductItem } from '../../_common/ProductItem/ProductItem';
 
 export const MyAuctionContent = ({ details, status }) => {
-  const viewDataFilter = details?.items.filter((el) => el.statusId === status + 1);
-
   return (
     <div className="w-full px-2.5 h-4/5 overflow-x-auto scrollbar-hide flex flex-col gap-2">
-      {viewDataFilter?.map((el) => {
-        return <ProductItem key={el.boardId} productDetail={el} />;
+      {details?.pages.map((page) => {
+        const filterData = page.items.filter((data) => data.statusId === status + 1);
+        return (
+          filterData[0] && (
+            <div key={crypto.randomUUID()} className="flex flex-col gap-2">
+              {filterData.map((el) => {
+                return <ProductItem key={el.boardId} productDetail={el} />;
+              })}
+            </div>
+          )
+        );
       })}
     </div>
   );
--- client/src/components/MyPage/MyAuctionList/MyAuctionList.tsx
+++ client/src/components/MyPage/MyAuctionList/MyAuctionList.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/dot-notation */
 import { useRecoilValue } from 'recoil';
-import { useState } from 'react';
-import { useQuery } from 'react-query';
+import { useEffect, useState } from 'react';
+import { useQuery, useInfiniteQuery } from 'react-query';
+import { useInView } from 'react-intersection-observer';
 import { SubHeader } from '../../_common/Header/SubHeader/SubHeader';
 import { TabBar } from '../../_common/TabBar/TabBar';
 import { MyAuctionBtn } from '../MyAuctionBtn/MyAuctionBtn';
@@ -11,22 +13,54 @@ import { accessTokenAtom } from '../../../atoms/token';
 import { Loading } from '../../_common/Loading/Loading';
 
 export const MyAuctionList = () => {
-  const [status, setStatus] = useState(0);
+  const [statusId, setStatusId] = useState(0);
   const token = useRecoilValue(accessTokenAtom);
+  const [isClick, setIsClick] = useState(true);
 
-  const { isLoading, error, data } = useQuery(
-    'myAuction',
-    () => myPageAPI.getMyAuctionList({ path: 'my-auction-list', token }),
-    { staleTime: 1000 * 20 }
+  // const { isLoading, error, data } = useQuery(
+  //   'myAuction',
+  //   () => myPageAPI.getMyAuctionList({ path: 'my-auction-list', token }),
+  //   { staleTime: 1000 * 20 }
+  // );
+
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+
+  const { data, status, hasNextPage, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
+    ['myAuction', `${statusId}`],
+    ({ pageParam = 1 }) => myPageAPI.getMyAuctionList({ path: 'my-auction-list', page: pageParam, token }),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const totalPage = lastPage['pageInfo']?.totalPages;
+        const nextPage = allPages.length;
+        return nextPage <= totalPage && nextPage;
+      },
+      enabled: isClick,
+      refetchOnMount: true,
+      refetchOnWindowFocus: true,
+    }
   );
+
+  useEffect(() => {
+    if (inView && hasNextPage) fetchNextPage();
+  }, [inView]);
 
   return (
     <div className="base-layout bg-white">
       <SubHeader>내가 등록한 경매</SubHeader>
-      <MyAuctionBtn status={status} setStatus={setStatus} />
-      {isLoading && <Loading />}
-      {error && <div>{`${AUCTION_STATUS[status]} 상품이 없습니다.`}</div>}
-      <MyAuctionContent details={data} status={status} />
+      <MyAuctionBtn status={statusId} setStatus={setStatusId} />
+      {status === 'loading' && <Loading />}
+      {status === 'error' && <div>{`${AUCTION_STATUS[statusId]} 상품이 없습니다.`}</div>}
+      <MyAuctionContent details={data} status={statusId} />
+      <button
+        type="button"
+        disabled={!hasNextPage || isFetchingNextPage}
+        className="text-opacity-20"
+        ref={ref}
+        onClick={() => fetchNextPage()}>
+        {isFetchingNextPage ? <Loading /> : hasNextPage ? 'more' : ''}
+      </button>
       <TabBar />
     </div>
   );
