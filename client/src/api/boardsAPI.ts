b1fadb8a39ba9e29184a2765c1ea1c109ee7f61a
Hyejung Park <hyejj19@naver.com>
2023-03-03 15:58:20 +0900

    chore : 주석 추가

--- client/src/api/boardsAPI.ts
+++ client/src/api/boardsAPI.ts
@@ -1,6 +1,8 @@
 import { ProductDetailResp } from '../types/product.type';
 import { httpClient } from '../utils/httpClient';
 
+// TODO: 메서드 정의 방식 화살표 => 단축문법으로 변경
+// TODO: 인터셉터 기능 완성되면 option 객체 및 token 주입 제거하기
 export const productDetailAPI = {
   url: `/boards`,
 
