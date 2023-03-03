b50f1faee957d4ffffbd024058d5bad98f971c3b
jaehoon <halmokme@gmail.com>
2023-03-03 15:24:49 +0900

    feat 로딩컴포넌트 간격주기

--- client/src/components/_common/Loading/Loading.tsx
+++ client/src/components/_common/Loading/Loading.tsx
@@ -1,5 +1,9 @@
 import { BeatLoader } from 'react-spinners';
 
 export const Loading = () => {
-  return <BeatLoader color="#e8e2e2" margin={4} size={16} speedMultiplier={0.6} />;
+  return (
+    <div className="p-2">
+      <BeatLoader color="#e8e2e2" margin={4} size={16} speedMultiplier={0.6} />
+    </div>
+  );
 };
