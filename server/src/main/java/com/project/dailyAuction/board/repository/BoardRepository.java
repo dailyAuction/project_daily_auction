e3fee307cffa6b767d8489c375767a17b187b257
woong-sung <cjswo138@gmail.com>
2023-03-11 15:36:21 +0900

    fix: 트랜잭션 추가

--- server/src/main/java/com/project/dailyAuction/board/repository/BoardRepository.java
+++ server/src/main/java/com/project/dailyAuction/board/repository/BoardRepository.java
@@ -8,9 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Transactional
 public interface BoardRepository extends JpaRepository<Board, Long> {
     void deleteBySellerId(long sellerId);
 
